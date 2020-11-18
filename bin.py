b0a73e9743ec7c37b7390ddb310112f4c308f101
KILL THE NET <33407405+KTN1990@users.noreply.github.com>
2020-11-18 15:21:57 +0100

    Update bin.py
    SOME BUGS ARE FIXED

--- bin.py
+++ bin.py
@@ -7,12 +7,17 @@ from sys import argv
 from prettytable import PrettyTable
 
 def _check_(cc):
+	p =b= None
 	T   = PrettyTable()
 	r 	= get(f"https://lookup.binlist.net/{cc}",
 		headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36', 
 		"Accept-Version": "3"}).json()
+	try: 
+		if r["prepaid"]: p = r["prepaid"]
+		if r["bank"]: b = f'{r["bank"]["name"]}({r["bank"]["url"]})'
+	except: pass
 	T.field_names = ['CARD NUM','CARD SCHEME', 'CARD TYPE', 'CARD BRAND', 'CARD PREPAID', 'CARD COUNTRY', 'CARD BANK']
-	T.add_row([cc,r["scheme"], r["type"], r["brand"], r["prepaid"], f'{r["country"]["name"]}({r["country"]["emoji"]} )', f'{r["bank"]["name"]}({r["bank"]["url"]})'])
+	T.add_row([cc,r["scheme"], r["type"], r["brand"], p, f'{r["country"]["name"]}({r["country"]["emoji"]} )', b])
 	print(T)
 
 if __name__ == '__main__':
@@ -28,6 +33,3 @@ if __name__ == '__main__':
 				if len(cc) >= 6: _check_(cc)
 		except KeyboardInterrupt: print("\nGOODBYE!"); exit()
 		except EOFError: print("\nGOODBYE!"); exit()
-
-
-
